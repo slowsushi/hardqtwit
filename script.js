6ef9315b609fdca1ae3e304e03107b80e1ea2e28
slowsushi <wuytsky@gmail.com>
2014-07-11 18:44:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 $.ajax({
-        url: "http://obscure-ridge-8042.herokuapp.com/",
+        url: "http://polar-retreat-1578.herokuapp.com",
         dataType: "json",
 
         success: function(data) {
           $.each(data.statuses, function () {
-            $( "#tweets" ).append('<div class="stuff1"><a target="_blank" href="klematis_big.htm"><img src="' + this['img'] + '" alt="Klematis" width="300" height="250"></a><div class="desc">' + this['name'] +  '</div></div>');
+            $( "#tweets" ).append('<div class="stuff1"><a target="_blank" href="klematis_big.htm"><img src="' + this['name'] + '" alt="Klematis" width="300" height="250"></a><div class="desc">' + this['name'] +  '</div></div>');
           });
        }});
