ecd89920db7eaa052d30146028a7551d8e7afc6f
slowsushi <wuytsky@gmail.com>
2014-07-12 18:26:17 +0100

    Update script.js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ $.ajax({
 
         success: function(data) {
           $.each(data.statuses, function () {
-            $( "#tweets" ).append('<div class="stuff1"><a target="_blank" href="klematis_big.htm"><img src="' + this['url'] + this ['name'] '" alt="Klematis" width="300" height="250"></a><div class="desc">' + this['name'] +  '</div></div>');
+            $( "#tweets" ).append('<div class="stuff1"><a target="_blank" href="klematis_big.htm"><img src="' + this['url'] + '" alt="Klematis" width="300" height="250"></a><div class="desc">' + this['name'] +  '</div></div>');
           });
        }});
